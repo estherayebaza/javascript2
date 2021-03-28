95078b9ea66d93b2d55370b70c94706f5d62d3bb
esther ayebaza <estherayebaza@gmail.com>
2021-03-28 20:35:18 +0300

    assignment.js

--- assignment.js
+++ assignment.js
@@ -1,15 +1,10 @@
-function getGrade(marks){
+var kiosk={
+        set getFruits(fruit){
+this.fruit=this.fruit+fruit;
+        },
+fruit:["apples","oranges","pineapple ,"]
+    }
+kiosk.getFruits="guavas";
+console.log(kiosk.fruit);
 
-    var marks=90;
-    
-    if (marks>=80){console.log("A");}
-    else if(marks>=70){console.log("B");}
-    else if(marks>=60)
-    {console.log("C");}
-    else if(marks>=50)
-    {console.log("D");}
-    else if(marks>=40)
-    {console.log("E");}
-    else {console.log("F");}
-    
-    }getGrade()
+
